6cf3e87b85363a1e6a36c07ce04757ee27a2f68e
G. Peter Lepage <g.p.lepage@cornell.edu>
2013-12-22 19:36:36 -0500

    formatting tweaks

--- examples/path-integral.py
+++ examples/path-integral.py
@@ -1,37 +1,29 @@
 """
-The most famous integral in quantum mechanics is the 
-Feynman path integral. In principle, the quantum 
-mechanics of any system (with a classical limit)
-can be reduced to a problem in multidimensional
-integration. Normally there are too many variables,
-and the integrand is too peaky to attempt using
-something like vegas. For a 1-d system, like the
-harmonic oscillator, however, it is feasible. 
+The most famous integral in quantum mechanics is the  Feynman path integral.
+In principle, the quantum  mechanics of any system (with a classical limit)
+can be reduced to a problem in multidimensional integration. Normally there
+are too many variables, and the integrand is too peaky to attempt using
+something like vegas. For a 1-d system, like the harmonic oscillator, however,
+it is feasible.
 
-The path integral has to be discretized (put on
-time lattice). A description of how this is done
-can be found in:
+The path integral has to be discretized (put on time lattice). A description
+of how this is done can be found in:
 
-G.P. Lepage, "Lattice QCD for Novices", 
+G.P. Lepage, "Lattice QCD for Novices",  
 at http://arxiv.org/abs/hep-lat/0506036 (June 2005).
 
-The code here uses a Cython integrand, path_integrand.pyx
-(for speed), and extracts the ground state energies  
-and wavefunctions for a regular harmonic oscillator,
-and also one with a small amount of anharmonicity 
---- just for fun. The results are not exactly 
-correct because we are using a coarse grid, 
+The code here uses a Cython integrand, path_integrand.pyx (for speed), and
+extracts the ground state energies and wavefunctions for a regular harmonic
+oscillator, and also one with a small amount of anharmonicity --- just for
+fun. The results are not exactly  correct because we are using a coarse grid,
 and also not taking T to infinity.
 
-Using numerical path integration to solve these 
-problems has got to be one of the most inefficient
-numerical approaches there is. One does it for the
-fun of experiencing the path integral as an actual
-integral (as opposed to a formal construct).
-Numerical path integration *is* the method of 
-choice, however, for solving really complicated,
-highly nonlinear problems like QCD; but one doesn't  
-use vegas to do this --- see the paper above.
+Using numerical path integration to solve these  problems has got to be one of
+the most inefficient numerical approaches there is. One does it for the fun of
+experiencing the path integral as an actual integral (as opposed to a formal
+construct). Numerical path integration *is* the method of  choice, however,
+for solving really complicated, highly nonlinear problems like QCD; but one
+doesn't   use vegas to do this --- see the paper above.
 """
 from __future__ import print_function   # makes this work for python2 and 3
 
