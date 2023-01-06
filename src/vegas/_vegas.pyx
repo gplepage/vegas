2b5a796b12067db322f092810d0d13d0b916fb74
G. Peter Lepage <g.p.lepage@cornell.edu>
2023-01-06 13:36:06 -0500

    documentation

--- src/vegas/_vegas.pyx
+++ src/vegas/_vegas.pyx
@@ -2,7 +2,7 @@
 # cython: profile=True
 
 # Created by G. Peter Lepage (Cornell University) in 12/2013.
-# Copyright (c) 2013-21 G. Peter Lepage.
+# Copyright (c) 2013-23 G. Peter Lepage.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
