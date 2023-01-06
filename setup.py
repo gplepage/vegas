be9a656e9d89131313b0c946ea192afd822c1bf1
G. Peter Lepage <g.p.lepage@cornell.edu>
2023-01-06 13:49:59 -0500

    tweak to setup.py

--- setup.py
+++ setup.py
@@ -107,7 +107,6 @@ setup(
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: POSIX',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
